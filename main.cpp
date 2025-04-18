ec7c9dd01d4dcc35aad632fe8cbd310f5ea2df0f
Tạ Quang Khôi <khoi0941@gmail.com>
2025-04-18 21:35:26 +0700

    Done run drogon with vcpkg

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
-// #include <drogon/drogon.h>
+#include <drogon/drogon.h>
 #include <fmt/core.h>
 
 int main() {
-    // drogon::app().loadConfigFile("../config.json");
-    // drogon::app().run();
-    fmt::print("Hello World!\n");
+    drogon::app().loadConfigFile("../config.json");
+    drogon::app().registerHandler(
+    "/hello",
+    [](const drogon::HttpRequestPtr& req,
+       std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+        auto resp = drogon::HttpResponse::newHttpResponse();
+        resp->setBody("Hello, Drogon!");
+        callback(resp);
+    });
+    drogon::app().registerHandler(
+    "/tan",
+    [](const drogon::HttpRequestPtr& req,
+       std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+        auto resp = drogon::HttpResponse::newHttpResponse();
+        resp->setBody("Hello, Tan!");
+        callback(resp);
+    });
+    drogon::app().run();
+    // fmt::print("Hello World!\n");
     return 0;
 }
