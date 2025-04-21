2dc505b0242a8eeb0178cb507b4cf0261f9db140
Tạ Quang Khôi <khoi0941@gmail.com>
2025-04-21 21:19:20 +0700

    understand the code

--- main.cpp
+++ main.cpp
@@ -1,25 +1,45 @@
 #include <drogon/drogon.h>
 #include <fmt/core.h>
 
-int main() {
+int main()
+{
     drogon::app().loadConfigFile("../config.json");
+
     drogon::app().registerHandler(
-    "/hello",
-    [](const drogon::HttpRequestPtr& req,
-       std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
-        auto resp = drogon::HttpResponse::newHttpResponse();
-        resp->setBody("Hello, Drogon!");
-        callback(resp);
-    });
+        "/",
+        [](
+        const drogon::HttpRequestPtr& req,
+        std::function<void(const drogon::HttpResponsePtr&)>&& callback
+    )
+        {
+            auto resp = drogon::HttpResponse::newHttpResponse();
+            resp->setBody("Hello, World!");
+
+            callback(resp);
+        }
+        );
+
     drogon::app().registerHandler(
-    "/tan",
-    [](const drogon::HttpRequestPtr& req,
-       std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
-        auto resp = drogon::HttpResponse::newHttpResponse();
-        resp->setBody("Hello, Tan!");
-        callback(resp);
-    });
+        "/hello",
+        [](const drogon::HttpRequestPtr& req,
+           std::function<void(const drogon::HttpResponsePtr&)>&& callback)
+        {
+            auto resp = drogon::HttpResponse::newHttpResponse();
+            resp->setBody("Hello, Drogon!");
+            callback(resp);
+        });
+
+    drogon::app().registerHandler(
+        "/tan",
+        [](const drogon::HttpRequestPtr& req,
+           std::function<void(const drogon::HttpResponsePtr&)>&& callback)
+        {
+            auto resp = drogon::HttpResponse::newHttpResponse();
+            resp->setBody("Hello, Tan!");
+            callback(resp);
+        });
+
     drogon::app().run();
-    // fmt::print("Hello World!\n");
+
     return 0;
 }
