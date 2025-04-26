af3bd33217b06b61d0f9e8921132a784d17422b6
Tạ Quang Khôi <khoi0941@gmail.com>
2025-04-26 21:23:43 +0700

    Add TanController with async HTTP request handling

---
+++ TanController.cc
@@ -0,0 +1,6 @@
+#include "TanController.h"
+
+void TanController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+}
