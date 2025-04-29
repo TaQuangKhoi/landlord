c72e7cec09b52556565671539b89bb52f1666ab3
Tạ Quang Khôi <khoi0941@gmail.com>
2025-04-29 20:56:00 +0700

    Add admin_UserController to CMakeLists.txt for build inclusion

---
+++ admin/admin_UserController.cc
@@ -0,0 +1,11 @@
+#include "admin_UserController.h"
+
+using namespace admin;
+
+void UserController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    std::string path = req->getPath();
+    auto resp = HttpResponse::newHttpResponse();
+    resp->setBody("Response for GET request to /adminUser from TanController");
+    callback(resp);
+}
