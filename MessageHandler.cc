ed03523b2b8d376b6cf9e9df0c2d03cf112c1baa
Kevin_Tan <donguyen.kevin.tan@gmail.com>
2025-05-05 20:50:24 +0700

    Update code

--- MessageHandler.cc
+++ MessageHandler.cc
@@ -4,7 +4,12 @@ void MessageHandler::postMessage(const HttpRequestPtr &req,
                                  std::function<void (const HttpResponsePtr &)> &&callback) {
     // Parse form-urlencoded data
     auto form = req->getParameter("content");  // Lấy nội dung form trường "content"
-    std::string responseText = "recived message: " + form;
+    auto user_name = req->getParameter("username");
+
+    // print debug
+    LOG_INFO << user_name;
+
+    std::string responseText = "recived message: " + form + user_name;
 
     auto resp = HttpResponse::newHttpResponse();
     resp->setContentTypeCode(CT_TEXT_PLAIN);
--- MessageHandler.h
+++ MessageHandler.h
@@ -6,8 +6,9 @@ using namespace drogon;
 class MessageHandler : public drogon::HttpController<MessageHandler> {
 public:
     METHOD_LIST_BEGIN
-        ADD_METHOD_TO(MessageHandler::postMessage, "/message", Post);
+        ADD_METHOD_TO(MessageHandler::postMessage, "/postMessage", Post);
     METHOD_LIST_END
+
 
     void postMessage(const HttpRequestPtr &req,
                      std::function<void (const HttpResponsePtr &)> &&callback);
