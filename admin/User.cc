ada8bc94d18bd2edaaf2100aed1937dbd0cdf8b3
Tạ Quang Khôi <khoi0941@gmail.com>
2025-05-05 19:58:59 +0700

    Rename admin_UserController to User and update CMakeLists.txt for inclusion

--- admin/User.cc
+++ admin/User.cc
@@ -1,8 +1,8 @@
-#include "admin_UserController.h"
+#include "User.h"
 
 using namespace admin;
 
-void UserController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+void UserController::asyncHandleHttpRequest(const drogon::HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
 {
     std::string path = req->getPath();
     auto resp = HttpResponse::newHttpResponse();
